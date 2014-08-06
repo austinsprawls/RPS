5f67ea302d33cd75ec715a3941e729af89ed7548
Daniel Cox <draycox@gmail.com>
2014-08-06 09:34:41 -0500

    Added sql table creation in dbi file.

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,13 @@
 module RPS
   class Player
-    attr_reader :id, :username, :email, :wins, :loss
+    attr_reader :id, :username, :email, :wins, :losses
 
     def initialize(args)
       @id = args[:id]
       @username = args[:username]
       @email = args[:email]
       @wins = 0
-      @loss = 0
+      @losses = 0
     end
   end
 end
